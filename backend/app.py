209de7d2635727499b11bf83b2271d93d8e51e7c
Yiran Wang <aurasoph@cs.washington.edu>
2024-11-10 00:36:11 -0800

    Minor updates for flask_cors

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 from pages.distribution import distribution_blueprint
 from pages.page2 import page2_blueprint 
 
@@ -6,8 +7,10 @@ app = Flask(
     __name__, 
     static_folder='../frontend/static',  
     static_url_path='/static', 
-    template_folder='../frontend/templates'  
+    template_folder='../frontend'  
 )
+
+CORS(app, resources={r"/*": {"origins": ["https://www.luntontius.site", "http://localhost:3000"]}})
 
 app.secret_key = 'mathtools'
 

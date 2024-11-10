8b2bb5bc392c1dd00972dea96adb8695ac6b41e5
Yiran Wang <aurasoph@cs.washington.edu>
2024-11-09 23:34:07 -0800

    Rearrangement

--- backend/app.py
+++ backend/app.py
@@ -4,10 +4,10 @@ from pages.distribution import distribution_blueprint
 from pages.page2 import page2_blueprint 
 
 app = Flask(
-    __name__, 
+    __name__,
     static_folder='../frontend/static',  
     static_url_path='/static', 
-    template_folder='../frontend/templates'  
+    template_folder='../frontend/'  
 )
 
 CORS(app, resources={r"/*": {"origins": "https://luntontius.site"}})
@@ -17,9 +17,6 @@ app.secret_key = 'mathtools'
 app.register_blueprint(distribution_blueprint, url_prefix='/distribution')
 app.register_blueprint(page2_blueprint, url_prefix='/page2')  
 
-@app.route('/')
-def index():
-    return render_template('index.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)

617c3def62cdf1879f59d52852b2a468947340f3
Yiran Wang <aurasoph@cs.washington.edu>
2024-11-09 23:53:26 -0800

    More rearrangement

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,22 @@
 from flask import Flask, render_template
-from flask_cors import CORS
 from pages.distribution import distribution_blueprint
 from pages.page2 import page2_blueprint 
 
 app = Flask(
-    __name__,
+    __name__, 
     static_folder='../frontend/static',  
     static_url_path='/static', 
-    template_folder='../frontend/'  
+    template_folder='../frontend/templates'  
 )
-
-CORS(app, resources={r"/*": {"origins": "https://luntontius.site"}})
 
 app.secret_key = 'mathtools'
 
 app.register_blueprint(distribution_blueprint, url_prefix='/distribution')
 app.register_blueprint(page2_blueprint, url_prefix='/page2')  
 
-#@app.route('/')
-#def index():
-#    return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)
